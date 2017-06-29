026275b3c47d3e14db0fac0fa15c2e1f971b5e6f
weloytty <bill@loytty.com>
2017-06-29 04:42:30 -0400

    Build Updates

--- Tests/Quirky.Module.Tests.ps1
+++ Tests/Quirky.Module.Tests.ps1
@@ -5,16 +5,16 @@
 Import-Module Quirky -Force
 
 Describe "Testing Aliases for Module" {
-  InModuleScope Quirky {
-    Context "Aliases" {
-	 $aliasValue = (Get-Alias slm -ErrorAction SilentlyContinue).Definition
-      It "Should know what the 'slm' Alias does" {
-        $aliasValue | Should Be "Set-LocationToModuleFolder"
-      }
-	        $aliasValue = (Get-Alias gms -ErrorAction SilentlyContinue).Definition
-      It "Should know what the 'gms' Alias does" {
-        $aliasValue | Should Be "Find-CommandSourceCode"
-      }
-	}
-	}
-	}
+    InModuleScope Quirky {
+        Context "Aliases" {
+            $aliasValue = (Get-Alias slm -ErrorAction SilentlyContinue).Definition
+            It "Should know what the 'slm' Alias does" {
+                $aliasValue | Should Be "Set-LocationToModuleFolder"
+            }
+            $aliasValue = (Get-Alias gms -ErrorAction SilentlyContinue).Definition
+            It "Should know what the 'gms' Alias does" {
+                $aliasValue | Should Be "Find-CommandSourceCode"
+            }
+        }
+    }
+}
