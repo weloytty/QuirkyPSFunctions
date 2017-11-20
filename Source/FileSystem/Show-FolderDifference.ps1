334077602c2bde1f681cf219fe7f92fde0d9d01b
Bill Loytty <loyttyw@corning.com>
2017-11-20 04:43:26 -0500

    Updated Show-FolderDifference

--- Source/FileSystem/Show-FolderDifference.ps1
+++ Source/FileSystem/Show-FolderDifference.ps1
@@ -1,13 +1,19 @@
 [CmdLetBinding()]
 param([string]$SourceFolder,
     [string]$TargetFolder,
-    [string[]]$FilesToExclude = @('*.xml', '*.config', '*.bak'))
+    [string[]]$FilesToExclude = @('*.xml', '*.config', '*.bak'),
+	[switch]$DisplayOnly,
+	[switch]$Quiet)
 
 begin {
     if (-not (Test-Path -Path $SourceFolder -PathType Container)) {throw "Can't find Source: $SourceFolder"}
     if (-not (Test-Path -Path $SourceFolder -PathType Container)) {throw "Can't find Source: $SourceFolder"}
     [array]$exclusions = $FilesToExclude 
     [hashtable]$returnValues = @{}
+	$numToCopy = 0
+	
+    if ($Quiet -and $DisplayOnly) {throw 'Invalid arguments: -DisplayOnly and -Quiet mean there will be zero output.'}
+
 }
 process {
 
@@ -18,12 +24,13 @@ process {
     $returnValues.Files = @()
 
 
-
+	[bool]$didSomething = $false
     $results = $(& "robocopy.exe" "$SourceFolder" "$TargetFolder" *.* /NJS /NJH /NS /XX /NDL /L $($exclusions.Split()))
     foreach ($line in $results) {
         Write-Verbose "Processing Line $line"
         if ($line.Length -gt 0) {
-            Write-Verbose "Line"
+			$numToCopy++
+			$didSomething = $true
             $fileClass = $line.Substring(0, 15).Trim()
             $fileName = $line.Substring(16).Trim()
             Write-Verbose "File : $fileName"
@@ -61,24 +68,34 @@ process {
 }
 
 end {
-    #return $returnValues
+	#return $returnValues
+	
+    if (-not $Quiet) {
+        Write-Host "Will copy $numToCopy files"
+    }
+
+
     foreach ($f in $returnValues.Files) {
 
-   
+        
+        if (-not $Quiet) {Write-Host $_.FullName}
+		
+        if (-not $DisplayOnly) {
        
-        $outObject = $( $f| Select-Object -property `
-            @{Label = "Class"; Expression = {($_.Class)}},
-            @{Label = "Name"; Expression = {($_.FullName)}},
-            @{Label = "Source Date"; Expression = {($_.SourceDate).ToshortDateString()}},
-            @{label = "Source Size"; Expression = {"{0:N2}" -f ($_.SourceSize / 1MB)}},
-            @{label = "Source MD5"; Expression = {$_.SourceChecksum}},
-            @{Label = "Target Date"; Expression = {($_.TargetDate).ToshortDateString()}},
-            @{label = "Target Size"; Expression = {"{0:N4}" -f ($_.TargetSize / 1MB)}},
-            @{label = "Target MD5"; Expression = {$_.TargetChecksum}})
+            $outObject = $( $f| Select-Object -property `
+                @{Label = "Class"; Expression = {($_.Class)}},
+                @{Label = "Name"; Expression = {($_.FullName)}},
+                @{Label = "Source Date"; Expression = {($_.SourceDate).ToshortDateString()}},
+                @{label = "Source Size"; Expression = {"{0:N2}" -f ($_.SourceSize / 1MB)}},
+                @{label = "Source MD5"; Expression = {$_.SourceChecksum}},
+                @{Label = "Target Date"; Expression = {($_.TargetDate).ToshortDateString()}},
+                @{label = "Target Size"; Expression = {"{0:N4}" -f ($_.TargetSize / 1MB)}},
+                @{label = "Target MD5"; Expression = {$_.TargetChecksum}})
 
       
 
-        Write-Output $outObject
+            Write-Output $outObject
+        }
        
     }
 }
