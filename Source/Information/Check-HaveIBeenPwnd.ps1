6f082e6298fa42d69c733e52888b668ed460d7b8
Bill Loytty <bill@loytty.com>
2018-03-19 20:02:40 -0400

    updates for hibp

--- Source/Information/Check-HaveIBeenPwnd.ps1
+++ Source/Information/Check-HaveIBeenPwnd.ps1
@@ -1,25 +1,27 @@
 [CmdletBinding()]
 param([string]$PwdToTest)
 
-begin{
+begin {
     Set-StrictMode -version Latest
 
     $requestString = "https://api.pwnedpasswords.com/range/"
 
-
-}
-process{
-
-
     $sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider 
     $toHash = [System.Text.Encoding]::UTF8.GetBytes($PwdToTest)
     $stringHash = [System.BitConverter]::ToString( $sha1.ComputeHash($toHash)).Replace("-", "")
+
+
+}
+process {
+    
     $prefix = $stringHash.substring(0, 5)
     $remainder = $stringHash.substring(5)
+
     Write-Verbose "URI      : $requestString"
     Write-Verbose "Prefix   : $prefix"
     write-verbose "Full URI : $requestString$prefix"
     Write-Verbose "Remainder: $remainder"
+
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $results = Invoke-WebRequest -Uri "$requestString$prefix" 
     
@@ -28,13 +30,8 @@ process{
         foreach ($s in $($results.Content -split "`r`n")) {
             $countofHash = $($s -split ":")[1]
             $stringToMatch = $($s -split ":")[0]
-            #Write-Verbose "String: $s"
-            #Write-Verbose "Match : $stringToMatch"
-            if($stringToMatch -eq $remainder){Write-Output "Password '$pwdToTest' has been used $countOfHash times."}
-        
+            if ($stringToMatch -eq $remainder) {Write-Output "Password '$pwdToTest' has been used $countOfHash times."}
         }
-        #$counter = 0
-        #$results.Content -split "`r`n"|% {$counter++;Write-Output "$counter $_"}
-    }else{Write-Output "No matches found"}
+    } else {Write-Output "No matches found"}
 
 }
