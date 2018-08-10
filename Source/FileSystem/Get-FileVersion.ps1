f05c4aa97dd0bc12554363640a147ae26baa0212
weloytty <bill@loytty.com>
2018-08-10 05:15:30 -0400

    Updated Get-FileVersion

--- Source/FileSystem/Get-FileVersion.ps1
+++ Source/FileSystem/Get-FileVersion.ps1
@@ -7,8 +7,8 @@
   process
   {
     foreach ($s in $filename) {
-      $ver = (Get-Item $s).VersionInfo.FileVersion
       if (Test-Path -Path $s) {
+        $ver = (Get-Item $s).VersionInfo.FileVersion
         $msg = "$s version $ver"
         if ($null -eq $ver ) { $msg = "$s contains no version info" }
         Write-Output $msg
