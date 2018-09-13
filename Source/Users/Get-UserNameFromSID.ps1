59b9fc5e26b3649f6247c7853b6dc037a486af84
weloytty <bill@loytty.com>
2018-09-13 09:08:44 -0400

    Add Get-UserNameFromSid and remove local user functions

---
+++ Source/Users/Get-UserNameFromSID.ps1
@@ -0,0 +1,18 @@
+    [CmdletBinding()]
+    param([string[]]$Sid)
+    begin {
+        Set-StrictMode -Version Latest
+    }
+    process {
+        foreach ($thisSid in $Sid) {
+            $objSID = New-Object System.Security.Principal.SecurityIdentifier($thisSid) 
+
+
+            $objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
+    
+            $objUser.Value
+        }
+    }
+    end {
+
+    }
