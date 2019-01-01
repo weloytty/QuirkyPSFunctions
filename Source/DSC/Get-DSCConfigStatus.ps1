76abf2bc50e98e3e1c3262861d4c41628b3c9f18
Bill Loytty <bill@loytty.com>
2019-01-01 18:17:51 -0500

    Removed obsolete New-QShare and formatting

--- Source/DSC/Get-DSCConfigStatus.ps1
+++ Source/DSC/Get-DSCConfigStatus.ps1
@@ -1,89 +1,72 @@
 
-  [CmdletBinding()]
-  param(
-	[Parameter(ValueFromPipeline = $true)]
-	[string[]]$ComputerName = $env:computername,
-	[switch]$Quiet,
-	[switch]$DisplayOnly,
-	[switch]$All
-  )
+[CmdletBinding()]
+param(
+    [Parameter(ValueFromPipeline = $true)]
+    [string[]]$ComputerName = $env:computername,
+    [switch]$Quiet,
+    [switch]$DisplayOnly,
+    [switch]$All
+)
 
-  begin {
-	Set-StrictMode -Version Latest
-  $returnValue = @()
-  }
-  process
-  {
-	foreach ($computer in $ComputerName)
-	{
-	  $session = Get-CimSession -ComputerName $Computer -ErrorAction SilentlyContinue
-        if ($null -ne $session)
-	  {
-		$session = New-CimSession -ComputerName $computer
-	  }
+begin {
+    Set-StrictMode -Version Latest
+    $returnValue = @()
+}
+process {
+    foreach ($computer in $ComputerName) {
+        $session = Get-CimSession -ComputerName $Computer -ErrorAction SilentlyContinue
+        if ($null -ne $session) {
+            $session = New-CimSession -ComputerName $computer
+        }
 
-	  if ($null -eq $session) { throw "Can't create session on $computer" }
-	  $allresults = Get-DscConfigurationStatus -CimSession $session -All:$All
-	  Remove-CimSession -CimSession $Session
+        if ($null -eq $session) { throw "Can't create session on $computer" }
+        $allresults = Get-DscConfigurationStatus -CimSession $session -All:$All
+        Remove-CimSession -CimSession $Session
 
-	  if ($null -ne $allresults) {
-		foreach ($results in $allresults)
-		{
-		  if (-not $Quiet)
-		  {
-			$paddedComputer = $computer.padRight(16)
+        if ($null -ne $allresults) {
+            foreach ($results in $allresults) {
+                if (-not $Quiet) {
+                    $paddedComputer = $computer.padRight(16)
 
-			Write-Host ""
-			Write-Host "$paddedComputer Configuration: $($results.Status) Type: $($results.Type) Reboot Needed: $($results.RebootRequested)"
-			if ($null -ne $results.NumberOfResources -and $results.NumberOfResources -gt 0)
-			{
+                    Write-Host ""
+                    Write-Host "$paddedComputer Configuration: $($results.Status) Type: $($results.Type) Reboot Needed: $($results.RebootRequested)"
+                    if ($null -ne $results.NumberOfResources -and $results.NumberOfResources -gt 0) {
 
-			  foreach ($ids in $results.ResourcesInDesiredState)
-			  {
-				if ($null -ne $ids) {
-				  $paddedConfiguration = $ids.ResourceID.padRight(40)
-				  Write-Host "  $paddedConfiguration In Desired State"
-				}
+                        foreach ($ids in $results.ResourcesInDesiredState) {
+                            if ($null -ne $ids) {
+                                $paddedConfiguration = $ids.ResourceID.padRight(40)
+                                Write-Host "  $paddedConfiguration In Desired State"
+                            }
 
-			  }
-			  foreach ($nds in $results.ResourcesNotInDesiredState)
-			  {
+                        }
+                        foreach ($nds in $results.ResourcesNotInDesiredState) {
 
-				if ($null -ne $nds)
-				{
+                            if ($null -ne $nds) {
 
 
-				  $outputString = "Config Unknown "
-				  if($null -ne $nds.ResourceId){$outputString = $nds.ResourceId}
+                                $outputString = "Config Unknown "
+                                if ($null -ne $nds.ResourceId) {$outputString = $nds.ResourceId}
 
 
 
-				  $paddedConfiguration = $outputString.padRight(40)
-				  Write-Host "  $paddedConfiguration " -NoNewline
-				  Write-Host "NOT In Desired State" -ForegroundColor Red
+                                $paddedConfiguration = $outputString.padRight(40)
+                                Write-Host "  $paddedConfiguration " -NoNewline
+                                Write-Host "NOT In Desired State" -ForegroundColor Red
 
 
-				}
-				#Write-Host ""
-			  }
+                            }
+                            #Write-Host ""
+                        }
 
-			}
-
-
-		  }
-		  $returnValue += $results
-		}
-
-	  }
-
-
-	}
-
-
-  }
-  end
-  {
-	if ($DisplayOnly) { $results = $null }
-	return,$returnValue
-  }
+                    }
+                }
+                $returnValue += $results
+            }
+        }
+    }
+}
+end {
+    if ($DisplayOnly) { $results = $null }
+    return, $returnValue
+}
 
--- Source/DSC/Show-WSManShells.ps1
+++ Source/DSC/Show-WSManShells.ps1
@@ -1,22 +1,19 @@
 
-  param([string[]]$ComputerName = "localhost",
-	[string[]]$UserName)
-  $results = @()
-  foreach ($computer in $ComputerName)
-  {
-	if (Test-NetConnection -ComputerName $Computer -InformationLevel Quiet)
-	{
-	  $shells = Get-WSManInstance -ConnectionUri "http:`/`/$($Computer):5985/wsman" shell -enum
-	  $results = $shells
-	  if ($null -ne $UserName -and $UserName.Length -gt 0) { $results = $null }
-	  foreach ($user in $UserName)
-	  {
-		$results += $($shells | Where-Object { $_.Owner -eq $user })
-	  }
+param([string[]]$ComputerName = "localhost",
+    [string[]]$UserName)
+$results = @()
+foreach ($computer in $ComputerName) {
+    if (Test-NetConnection -ComputerName $Computer -InformationLevel Quiet) {
+        $shells = Get-WSManInstance -ConnectionUri "http:`/`/$($Computer):5985/wsman" shell -enum
+        $results = $shells
+        if ($null -ne $UserName -and $UserName.Length -gt 0) { $results = $null }
+        foreach ($user in $UserName) {
+            $results += $($shells | Where-Object { $_.Owner -eq $user })
+        }
 
-	} else { Write-Output "Can't connect to $computer" }
+    } else { Write-Output "Can't connect to $computer" }
 
-  }
-  return $results
+}
+return $results
 
 
